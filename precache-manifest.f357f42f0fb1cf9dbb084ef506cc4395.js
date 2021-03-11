b326f5469ea4d5d3ba1811cdf86922120b8713db
Abdulrehman55 <abdulrehmanmunaf123@gmail.com>
2021-03-11 18:16:10 +0500

    Updates

---
+++ precache-manifest.f357f42f0fb1cf9dbb084ef506cc4395.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4cb210ec8a07710793e97b74ed2c807",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "5e4458dc3f12b9b659a3",
+    "url": "/ToDoApp/static/js/2.0a003db7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoApp/static/js/2.0a003db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e02194aa8ec3f8f52d9",
+    "url": "/ToDoApp/static/js/main.af2208f2.chunk.js"
+  },
+  {
+    "revision": "756a6794fd4ab5e4f294",
+    "url": "/ToDoApp/static/js/runtime-main.9a39882d.js"
+  }
+]);
